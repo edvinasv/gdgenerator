d4191891ef5e35738fa33fef0424c9854aabf45e
Edvinas Vyzas <edvinas.vyzas@gmail.com>
2025-09-28 14:24:13 +0300

    Generate Alert Rules

--- main.go
+++ main.go
@@ -25,6 +25,7 @@ type DataRowColumnGroupItem struct {
 	Max           float64  `yaml:"max"`
 	DatasourceUid string   `yaml:"datasourceUid"`
 	Type          string   `yaml:"type"`
+	Service       string   `yaml:"service"`
 	Disabled      bool     `yaml:"disabled"`
 }
 
@@ -38,6 +39,7 @@ type DataRowColumnGroup struct {
 	Columns       uint32                   `yaml:"columns"`
 	DatasourceUid string                   `yaml:"datasourceUid"`
 	Type          string                   `yaml:"type"`
+	Service       string                   `yaml:"service"`
 	Disabled      bool                     `yaml:"disabled"`
 	Items         []DataRowColumnGroupItem `yaml:"items"`
 }
@@ -46,6 +48,7 @@ type DataRowColumn struct {
 	Title         string               `yaml:"title"`
 	DatasourceUid string               `yaml:"datasourceUid"`
 	Type          string               `yaml:"type"`
+	Service       string               `yaml:"service"`
 	Disabled      bool                 `yaml:"disabled"`
 	Groups        []DataRowColumnGroup `yaml:"groups"`
 }
@@ -55,6 +58,7 @@ type DataRow struct {
 	Columns       []DataRowColumn `yaml:"columns"`
 	DatasourceUid string          `yaml:"datasourceUid"`
 	Type          string          `yaml:"type"`
+	Service       string          `yaml:"service"`
 	Disabled      bool            `yaml:"disabled"`
 }
 
@@ -66,42 +70,83 @@ type DashboardData struct {
 	Refresh       string                 `yaml:"refresh"`
 	DatasourceUid string                 `yaml:"datasourceUid"`
 	Type          string                 `yaml:"type"`
+	Service       string                 `yaml:"service"`
 	Disabled      bool                   `yaml:"disabled"`
 	Banner        string                 `yaml:"banner"`
 	DryRun        bool                   `yaml:"dryRun"`
 	Rows          []DataRow              `yaml:"rows"`
 }
 
+type AlertLabels struct {
+	Severity string `yaml:"severity,omitempty"`
+	Service  string `yaml:"service,omitempty"`
+}
+
+type AlertAnnotations struct {
+	Summary string `yaml:"summary"`
+}
+
+type AlertRule struct {
+	Alert                 string           `yaml:"alert"`
+	Expr                  string           `yaml:"expr"`
+	ForDuration           string           `yaml:"for,omitempty"`
+	KeepFiringForDuration string           `yaml:"keep_firing_for,omitempty"`
+	Labels                AlertLabels      `yaml:"labels"`
+	Annotations           AlertAnnotations `yaml:"annotations"`
+}
+
+type AlertGroup struct {
+	Name     string      `yaml:"name"`
+	Interval string      `yaml:"interval,omitempty"`
+	Rules    []AlertRule `yaml:"rules"`
+}
+
+type AlertGroups struct {
+	Groups []AlertGroup `yaml:"groups"`
+}
+
 func main() {
 
 	manifests := false
 	manifestsDirectory := "./resources"
+	alerts := false
+	alertsDirectory := "./alerts"
 	configFile := "examples/generic.yaml"
 
 	var dashboardData DashboardData
 
-	flag.BoolVar(&manifests, "manifests", manifests, "Generate a dashboard manifest for the test dashboard and write it to disk")
+	flag.BoolVar(&manifests, "manifests", manifests, "Generate a dashboard manifest and write it to disk")
 	flag.StringVar(&manifestsDirectory, "manifests-directory", manifestsDirectory, "Directory in which the manifests will be generated")
+	flag.BoolVar(&alerts, "alerts", alerts, "Generate alert configuration and write it to disk")
+	flag.StringVar(&alertsDirectory, "alerts-directory", alertsDirectory, "Alerts configuration directory")
 	flag.StringVar(&configFile, "config", configFile, "Dashboard configuration file")
 	flag.Parse()
 
 	dashboardData.getConf(configFile)
 
-	genericDashboard, err := generateDashboard(dashboardData).Build()
+	dashboard, err := generateDashboard(dashboardData).Build()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// Generate a dashboard manifest for the generic dashboard and write it to disk.
 	if manifests {
-		if err := generateManifest(dashboardData.FolderUid, manifestsDirectory, genericDashboard); err != nil {
+		if err := generateDashboardManifest(dashboardData.FolderUid, manifestsDirectory, dashboard); err != nil {
 			log.Fatal(err)
 		}
 		return
 	}
 
-	// By default: print the test dashboard to stdout.
-	printDashboard(genericDashboard)
+	if alerts {
+		alertGroups := generateAlerts(dashboardData)
+		fmt.Println("%+v\n", alertGroups)
+		if err := generateAlertGroupFile(alertsDirectory, dashboard, alertGroups); err != nil {
+			log.Fatal(err)
+		}
+		return
+	}
+
+	printDashboard(dashboard)
+
 }
 
 func (c *DashboardData) getConf(configFile string) *DashboardData {
@@ -122,7 +167,7 @@ func (c *DashboardData) getConf(configFile string) *DashboardData {
 	return c
 }
 
-func generateManifest(folderUid string, outputDir string, dashboard dashboard.Dashboard) error {
+func generateDashboardManifest(folderUid string, outputDir string, dashboard dashboard.Dashboard) error {
 
 	if err := os.MkdirAll(outputDir, 0777); err != nil {
 		return err
@@ -140,6 +185,25 @@ func generateManifest(folderUid string, outputDir string, dashboard dashboard.Da
 	}
 
 	return nil
+}
+
+func generateAlertGroupFile(outputDir string, dashboard dashboard.Dashboard, alertGroups *AlertGroups) error {
+	if err := os.MkdirAll(outputDir, 0777); err != nil {
+		return err
+	}
+
+	alertConfig, err := yaml.Marshal(alertGroups)
+	if err != nil {
+		return err
+	}
+
+	filename := *dashboard.Uid + ".rules.yaml"
+	if err := os.WriteFile(filepath.Join(outputDir, filename), alertConfig, 0666); err != nil {
+		return err
+	}
+
+	return nil
+
 }
 
 func printDashboard(dashboard dashboard.Dashboard) {
